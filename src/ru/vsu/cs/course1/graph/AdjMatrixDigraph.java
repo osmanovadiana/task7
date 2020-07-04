d189a120664ccd4d7adbb762817056537317448b
osmanovadiana <diana.osmanova36@gmail.com>
2020-07-04 14:16:23 +0300

    Initial commit

---
+++ src/ru/vsu/cs/course1/graph/AdjMatrixDigraph.java
@@ -0,0 +1,34 @@
+package ru.vsu.cs.course1.graph;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Реализация ориентированного графа (орграфа) на основе матрицы смежности
+ */
+public class AdjMatrixDigraph extends AdjMatrixGraph implements Digraph {
+    /**
+     * Реализация полность совпадает
+     * (в AdjMatrixGraph специально учтено)
+     */
+
+
+
+    /**
+     * Конструктор
+     * @param vertexCount Кол-во вершин графа (может увеличиваться при добавлении дуг)
+     */
+    public AdjMatrixDigraph(int vertexCount) {
+        super(vertexCount);
+    }
+
+    /**
+     * Конструктор без парметров
+     * (лучше не использовать, т.к. при добавлении вершин каждый раз пересоздается матрица)
+     */
+    public AdjMatrixDigraph() {
+        this(0);
+    }
+}
